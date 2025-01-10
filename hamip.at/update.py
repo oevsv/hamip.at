5d027e92dec9f3173deef431b645191e9fc7a714
Dietmar Zlabinger <dietmar@zlabinger.at>
2025-01-10 16:16:37 +0100

    Include hamnet instance

--- hamip.at/update.py
+++ hamip.at/update.py
@@ -7,11 +7,17 @@ from powerdns_util import *
 
 # https://doc.powerdns.com/authoritative/http-api/zone.html
 
-API_ENDPOINT_ISP = "https://dnsapi.netplanet.at/api"
+api_endpoint = "https://dnsapi.netplanet.at/api"
 API_KEY_ISP_LOCATION = "/etc/hamip/key.asc"
 API_ENDPOINT_HAMNET = "http://127.0.0.1:8081/api"
 API_KEY_HAMNET_LOCATION = "/etc/hamip/key_hamnet.asc"
 HAMIP_AT = '.hamip.at.'
+
+static_dict = {
+    "*": Resource_record(content="89.185.96.125", type="A", ttl=600),
+    "hamip.at.": Resource_record(content="89.185.96.125", type="A", ttl=600)
+}
+
 
 DEBUG = False
 
@@ -58,7 +64,7 @@ def print_response(response):
 
 
 
-def prepare_patch(task, delete, api_endpoint):
+def prepare_patch(task, delete, api_endpoint,api_key):
     CHUNK_SIZE = 500
 
     # split into chunks
@@ -102,6 +108,77 @@ def prepare_patch(task, delete, api_endpoint):
 
         request_patch(api_endpoint, rrset_payload, 204, api_key)
 
+def process_powerdns(api_endpoint, api_key):
+    zone_id = get_zones(api_endpoint, api_key)
+
+    response = get_zone(api_endpoint, api_key)
+    data = response.json()
+
+    # Retrieve the serial number
+    serial = data.get('serial', None)
+    print(f"Current serial: {serial}")
+
+    if serial == None:
+        print("No serial,failed")
+        print_response(response)
+        exit(-1)
+
+    # Retrieve the serial number
+    edited_serial = data.get('edited_serial', None)
+    if DEBUG:
+        print(f"Current edited_serial: {edited_serial}")
+
+    if edited_serial == None:
+        print("No edited_serial,failed")
+        print_response(response)
+        exit(1)
+
+    rrsets_dict_on_server = get_current_zone(zone_id, api_endpoint, api_key)
+    # print("rrsets_dict_on_server")
+    # print(rrsets_dict_on_server)
+
+    # compare hamnetdb_dict_name_ip and hamnetdb_dict_aliases_cname with rrsets_dict_on_server
+
+    # Dictionaries to hold removals and changes/additions
+    to_remove = {}
+    to_change = {}
+
+
+
+    # Process removal
+
+    for key in rrsets_dict_on_server:
+        if (key not in hamnetdb_dict
+                and key not in static_dict):
+            to_remove[key] = rrsets_dict_on_server[key]
+
+    print("Keys to be removed:", len(to_remove))
+    # print(to_remove)
+
+    prepare_patch(to_remove, True, api_endpoint,api_key)
+
+    # Identify keys to change/add
+    for key in hamnetdb_dict:
+        # if new or changed
+        key_not_on_server = key not in rrsets_dict_on_server
+        # add if not there
+        if key_not_on_server:
+            to_change[key] = hamnetdb_dict[key]
+        elif rrsets_dict_on_server[key] != hamnetdb_dict[key]:
+            to_change[key] = hamnetdb_dict[key]
+
+    print("Keys to be changed or added:", len(to_change))
+
+    prepare_patch(to_change, False, api_endpoint, api_key)
+
+    ENABLE_UPDATE_SERIAL = True
+    if ENABLE_UPDATE_SERIAL:
+        update_serial(api_endpoint, api_key)
+
+    if len(to_remove) > 0 or len(to_change) > 0 and DEBUG:
+        # getupdated zone
+        get_zone(api_endpoint, api_key)
+        print_response(response)
 
 
 # main
@@ -117,82 +194,16 @@ if USE_DHCP:
     get_hamnetdb_dhcp(dhcp_dict, hamnetdb_dict, HAMIP_AT)
     hamnetdb_dict = hamnetdb_dict | dhcp_dict
 
-api_key = read_auth_key(API_KEY_ISP_LOCATION)
-if api_key is None:
+api_key_isp = read_auth_key(API_KEY_ISP_LOCATION)
+if api_key_isp is None:
     print(f"Error: Key not found at", API_KEY_ISP_LOCATION, "or could not be read.")
     sys.exit(1)  # Exit with a non-zero status code
-# print(api_key)
 
-zone_id = get_zones(API_ENDPOINT_ISP, api_key)
+api_key_hamnet = read_auth_key(API_KEY_HAMNET_LOCATION)
+if api_key_hamnet is None:
+    print(f"Error: Key not found at", API_KEY_HAMNET_LOCATION, "or could not be read.")
+    sys.exit(1)  # Exit with a non-zero status code
 
-response = get_zone(API_ENDPOINT_ISP, api_key)
-data = response.json()
 
-# Retrieve the serial number
-serial = data.get('serial', None)
-print(f"Current serial: {serial}")
-
-if serial == None:
-    print("No serial,failed")
-    print_response(response)
-    exit(-1)
-
-# Retrieve the serial number
-edited_serial = data.get('edited_serial', None)
-if DEBUG:
-    print(f"Current edited_serial: {edited_serial}")
-
-if edited_serial == None:
-    print("No edited_serial,failed")
-    print_response(response)
-    exit(1)
-
-rrsets_dict_on_server = get_current_zone(zone_id, API_ENDPOINT_ISP, api_key)
-# print("rrsets_dict_on_server")
-# print(rrsets_dict_on_server)
-
-# compare hamnetdb_dict_name_ip and hamnetdb_dict_aliases_cname with rrsets_dict_on_server
-
-# Dictionaries to hold removals and changes/additions
-to_remove = {}
-to_change = {}
-
-static_dict = {
-    "*": Resource_record(content="89.185.96.125", type="A", ttl=600),
-    "hamip.at.": Resource_record(content="89.185.96.125", type="A", ttl=600)
-}
-
-# Process removal
-
-for key in rrsets_dict_on_server:
-    if (key not in hamnetdb_dict
-            and key not in static_dict):
-        to_remove[key] = rrsets_dict_on_server[key]
-
-print("Keys to be removed:", len(to_remove))
-# print(to_remove)
-
-prepare_patch(to_remove, True, API_ENDPOINT_ISP)
-
-# Identify keys to change/add
-for key in hamnetdb_dict:
-    # if new or changed
-    key_not_on_server = key not in rrsets_dict_on_server
-    # add if not there
-    if key_not_on_server:
-        to_change[key] = hamnetdb_dict[key]
-    elif rrsets_dict_on_server[key] != hamnetdb_dict[key]:
-        to_change[key] = hamnetdb_dict[key]
-
-print("Keys to be changed or added:", len(to_change))
-
-prepare_patch(to_change, False, API_ENDPOINT_ISP)
-
-ENABLE_UPDATE_SERIAL = True
-if ENABLE_UPDATE_SERIAL:
-    update_serial(API_ENDPOINT_ISP, api_key)
-
-if len(to_remove) > 0 or len(to_change) > 0 and DEBUG:
-    # getupdated zone
-    get_zone(API_ENDPOINT_ISP, api_key)
-    print_response(response)
+process_powerdns(api_endpoint, api_key_isp)
+process_powerdns(API_ENDPOINT_HAMNET, api_key_hamnet)
